52942af00235adc00e20fde68d11eefd6ce06287
madhwan-phadtare <madhwanphadtere.123@gmail.com>
2023-08-10 23:32:06 +0530

    Initial Commit

--- src/main/java/com/cwm/FeedIndia/FeedIndiaApplication.java
+++ src/main/java/com/cwm/FeedIndia/FeedIndiaApplication.java
@@ -8,6 +8,7 @@ public class FeedIndiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeedIndiaApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
