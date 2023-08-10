b2c32e13d98a36fc573dbc4f3a9915896b880e54
madhwan.phadtare <madhwan.phadtare@quantiphi.com>
2023-08-10 23:05:26 +0530

    initial commit

---
+++ src/main/java/com/cwm/FeedIndia/FeedIndiaApplication.java
@@ -0,0 +1,13 @@
+package com.cwm.FeedIndia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedIndiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedIndiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwm/FeedIndia/FeedIndiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwm.FeedIndia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedIndiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
